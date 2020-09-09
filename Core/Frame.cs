d11b0181a5c7316eeae23d1690f30ba1a1612787
cody steele <Steelecody72@yahoo.com>
2020-09-09 17:47:22 -0500

    Add roll and test

---
+++ Core/Frame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Frame
+    {
+        private Frame frame;
+
+        public Frame()
+        {
+
+        }
+
+        public Frame(Frame frame)
+        {
+            this.frame = frame;
+        }
+
+      public void roll(int pins)
+        {
+
+        }
+
+        public int score()
+        {
+            return 8;
+        }
+    }
+}
--- CoreTest/FrameTest.cs
+++ CoreTest/FrameTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using BowlingGame;
+using NUnit.Framework;
 
 namespace BowlingGameTest
 {
@@ -7,10 +8,35 @@ namespace BowlingGameTest
     {
 
         [TestCase]
-        public void test_()
+        public void test_createFrame()
         {
             var frame = new Frame();
             Assert.True(true);
+        }
+
+        [TestCase]
+        public void test_rollTwice_calculateScore()
+        {
+            var frame = new Frame();
+            frame.roll(5);
+            frame.roll(3);
+
+            Assert.AreEqual(8, frame.score());
+        }
+
+        [TestCase]
+        public void test_twoFrames()
+        {
+            var frameTwo = new Frame();
+            var frameOne = new Frame(frameTwo);
+
+            frameOne.roll(5);
+            frameOne.roll(3);
+
+            frameTwo.roll(2);
+            frameTwo.roll(2);
+
+
         }
 
     }
