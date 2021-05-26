a4f75066a6211e7f9e1a9446b35d550b7ad154ba
Devin <devinallen26@gmail.com>
2021-05-26 17:18:24 -0500

    Fitnesse Fixture is ... almost .. working

---
+++ BowlingGameFixture/GutterGameFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using fit;
+using BowlingGame;
+
+public class GutterGameFixture : ColumnFixture
+{
+    private Game game;
+    private int roll;
+
+    public GutterGameFixture()
+    {
+        this.game = new Game(new FrameFactory());
+        this.roll = 0;
+    }
+
+    public void Roll(int roll)
+    {
+        game.Roll(roll);
+    }
+
+    public int Score()
+    {
+        return game.Score();
+    }
+}
