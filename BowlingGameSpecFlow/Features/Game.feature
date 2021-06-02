68600d3b59052d37240d6f1fefd2a71764a0d630
Cole Garien <colegarien@gmail.com>
2021-06-02 17:17:13 -0500

    scaffold in the three acceptance tests (still need to actually write the gherkin and implement the step definitons)

---
+++ BowlingGameSpecFlow/Features/Game.feature
@@ -0,0 +1,19 @@
+﻿Feature: Game
+
+Scenario: Gutter Game
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+	
+Scenario: Sporadic Game
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+	
+Scenario: Perfect Game
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
---
+++ BowlingGameSpecFlow/Steps/GameStepDefinitions.cs
@@ -0,0 +1,42 @@
+﻿using TechTalk.SpecFlow;
+
+namespace BowlingGameSpecFlow.Steps
+{
+    [Binding]
+    public sealed class GameStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public GameStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        // TODO: Implement the Gheirkin for Gutter, Sporadic, and Perfect game Acceptance TEsts -> https://github.com/colegarien/the-thing/wiki
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+
+            _scenarioContext.Pending();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
